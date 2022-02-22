b021142afc81e782403d83b85ab876d240fe43a4
Jonathan Sivahera <jsivahera@icloud.com>
2022-02-22 09:59:48 +0200

    Revert "Code review exercise"

--- src/task.js
+++
@@ -1,7 +0,0 @@
-export default class Task {
-  constructor(description, container) {
-    this.description = description;
-    this.completed = false;
-    this.index = container.length + 1;
-  }
-}
